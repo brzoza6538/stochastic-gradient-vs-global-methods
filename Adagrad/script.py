a342b6ad88ac5fe5546ffb7190f29c7aac17aff6
Michalbrz <01178515@pw.edu.pl>
2025-05-21 10:36:16 +0200

    logs for clamps

--- Adagrad/script.py
+++ Adagrad/script.py
@@ -2,8 +2,10 @@ import numpy as np
 from algorithms import globals
 from algorithms import Adagrad
 import time
+from functools import partial
 
-def run_Adagrad(dimension, curr_f, run_id, seed=None, lr=None):
+
+def run_Adagrad(dimension, curr_f, run_id, seed=None, lr=0.001):
     seed = seed or int((time.time() * 1000) + run_id)  # Generujemy nasiono na podstawie czasu i run_id
     seed = seed % (2**32)
     np.random.seed(seed)
@@ -13,10 +15,8 @@ def run_Adagrad(dimension, curr_f, run_id, seed=None, lr=None):
     x = np.random.uniform(globals.def_clamps[0], globals.def_clamps[1], size=dimension)
     eval = globals.Evaluation_method(curr_f, dimension)
 
-    if (lr is not None):
-        alg = Adagrad(eval.evaluate, eval.gradient, dimension, x=x, lr=lr)
-    else:
-        alg = Adagrad(eval.evaluate, eval.gradient, dimension, x=x)
+    alg = Adagrad(eval.evaluate, eval.gradient, dimension, x=x, B=lr)
+
     alg.start()
     log = alg.log
     for checkpoint in log.keys():
@@ -29,5 +29,7 @@ def run_Adagrad(dimension, curr_f, run_id, seed=None, lr=None):
             })
     return result
 
-globals.gather_data(run_Adagrad, "adagrad_clamp_lr=0.001")
-globals.gather_data(run_Adagrad, "adagrad_clamp_lr=0.01")
+
+globals.gather_data(partial(run_Adagrad, lr=0.001), "adagrad_clamp_lr=0.001")
+globals.gather_data(partial(run_Adagrad, lr=0.01), "adagrad_clamp_lr=0.01")
+globals.gather_data(partial(run_Adagrad, lr=0.1), "adagrad_clamp_lr=0.1")
